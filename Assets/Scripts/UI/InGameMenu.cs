a2586e7c147962b6bb1c67d6b61cc162bd9be0d7
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2019-09-10 19:14:21 +0900

    InGameMenu
    Inventory, Achievement, Story

---
+++ Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+    public GameObject[] Menus;
+
+    int Focused = 0;
+
+    GameObject _Player;
+
+    private void Awake()
+    {
+        _Player = GameObject.Find("Player");
+    }
+
+    private void Start()
+    {
+        Menus[Focused].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) { CloseInGameMenu(); }
+        if (Input.GetKeyDown(KeyCode.U)) { ChangeMenu(-1); }
+        if (Input.GetKeyDown(KeyCode.O)) { ChangeMenu(1); }
+    }
+
+    void CloseInGameMenu()
+    {
+        // _Player.GetComponent<PlayerController>().enabled = true;
+        Focused = 0;
+        gameObject.SetActive(false);
+    }
+
+    void ChangeMenu(int AdjustValue)
+    {
+        if(!(Focused + AdjustValue < 0 || Focused + AdjustValue >= Menus.Length))
+        {
+            Menus[Focused + AdjustValue].SetActive(true);
+            Menus[Focused].SetActive(false);
+            Focused += AdjustValue;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] Slot;
+    public bool[] isFull;
+
+    GameObject _Player;
+    int Focused = 0;
+
+    private void Awake()
+    {
+        Slot[Focused].transform.GetChild(0).gameObject.SetActive(true);
+        _Player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) { CloseInventory(); }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { FocusedSlot(1); }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { FocusedSlot(-1); }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { FocusedSlot(6); }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { FocusedSlot(-6); }
+    }
+
+    void CloseInventory()
+    {
+        // _Player.GetComponent<PlayerController>().enabled = true;
+        gameObject.SetActive(false);
+    }
+
+    void FocusedSlot(int AdjustValue)
+    {
+        if (Focused + AdjustValue < 0 || Focused + AdjustValue > 23) { return; }
+
+        Slot[Focused].transform.GetChild(0).gameObject.SetActive(false);
+        Focused += AdjustValue;
+        Slot[Focused].transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
