b100064a143625f1c8ecc8b87ee2553369cc3743
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2019-09-18 11:19:02 +0900

    Item
    Looting.cs, DropItemList.cs

---
+++ Assets/Scripts/UI/Items/DropItemList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 몬스터 프리팹에 스크립트를 추가
+ * 드랍될 아이템프리팹과 드랍확률을 인스펙터창에서 설정
+ * 몬스터가 죽을때 ItemDropChance() 호출
+ * */
+
+public class DropItemList : MonoBehaviour
+{
+    public GameObject[] DropItems;
+    public int[] DropChances;
+
+    public void ItemDropChance()
+    {
+        int DropChance = Random.Range(1, 101);
+
+        for(int i=0; i<DropItems.Length; i++)
+        {
+            if(DropChance > DropChances[i])
+            {
+                Instantiate(DropItems[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Items/Looting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * 필드 드랍 아이템 프리팹에 스크립트 추가
+ * */
+
+public class Looting : MonoBehaviour
+{
+    Inventory _Invenroy;
+
+    private void Start()
+    {
+        _Invenroy = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            for(int i=0; i<_Invenroy.Slot.Length; i++)
+            {
+                if(_Invenroy.isFull[i] == false)
+                {
+                    _Invenroy.isFull[i] = true;
+                    _Invenroy.Slot[i].GetComponent<Image>().sprite = GetComponent<SpriteRenderer>().sprite;
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
