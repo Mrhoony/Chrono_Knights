8468287bdf0bc2c8749ade4486e40d0c8f5425a9
LAB2LINCH <Labyues@gmail.com>
2020-03-13 03:30:21 +0900

    Update bell system

--- Assets/Script/UI/MainUI_PlayerStatusView.cs
+++ Assets/Script/UI/MainUI_PlayerStatusView.cs
@@ -15,11 +15,21 @@ public class MainUI_PlayerStatusView : MonoBehaviour
     public Image[] buffState;
 
     public Image bell;
-    public bool speedDown;
-    public float arrow;
-    public float power;
-    public float hitCount;
     public float currentHP;
+
+    private static float maxBellRotation = 80;
+    private static float doublemaxBellRotation = 6400;
+    private float beforeRotation;
+    public float targetRotation;
+    private float bellRotation;
+    private float bellPower;
+    private float bellPower2;
+    private float bellspd;
+    private bool isright;
+
+    public float ringTime;
+    public float dmgMulti;
+    public float dmgRecovery;
 
     IEnumerator monsterHit;
 
@@ -30,11 +40,19 @@ public class MainUI_PlayerStatusView : MonoBehaviour
 
     private void Start()
     {
-        speedDown = false;
-        arrow = 0;          // 흔들리는 방향
-        power = 0;          // 흔들리는 힘
-        hitCount = 0;       // 맞은 횟수 카운트
         monsterHit = MonsterHit();
+
+        bellRotation = 0;
+        beforeRotation = 0;
+        targetRotation = 0;
+        bellPower = 0;
+        bellPower2 = 0;
+        bellspd = 0;
+        ringTime = 4;
+        isright = true;
+
+        dmgMulti = 5;
+        dmgRecovery = 5;
     }
 
     public void Init()
@@ -51,56 +69,74 @@ public class MainUI_PlayerStatusView : MonoBehaviour
     {
         // 체력바 갱신
         HPBar.fillAmount = playerStatus.currentHP / playerStatus.playerData.GetStatus((int)PlayerStat.HP);
-        buffBar.fillAmount = playerStatus.currentAmmo / playerStatus.playerData.GetMaxAmmo();
+        //buffBar.fillAmount = playerStatus.currentAmmo / playerStatus.playerData.GetMaxAmmo();
 
-        // 맞은 횟수 비례 최대 흔들리는 각도
-        if (bell.transform.rotation.z * 90f > 0.2f * hitCount)
+        //로테이션값 오일러값으로 변경
+        bellRotation = bell.transform.rotation.eulerAngles.z;
+        if (180 <= bellRotation) bellRotation -= 360;
+
+        //각도 끝에 도달한경우 반대방향 힘 재계산
+        if (0 != targetRotation)
         {
-            if (power > 0)
+            if ((true == isright && bellRotation >= beforeRotation) || (false == isright && bellRotation <= (-1*beforeRotation)))
             {
-                arrow = -0.1f * hitCount;
-            }
-        }else if(bell.transform.rotation.z * 90f < -0.2f * hitCount)
-        {
-            if (power < 0)
-            {
-                arrow = 0.1f * hitCount;
+                //Debug.Log("Rotation : " + bellRotation + "   beforeRotation : " + beforeRotation);
+                //로테이션값 0일때 강제로 이동시킴
+                if (1 >= bellRotation * bellRotation) 
+                {
+                    if (true == isright)
+                    {
+                        bellRotation = 1;
+                        bell.transform.Rotate(new Vector3(0, 0, bellRotation));
+                    }
+                    if (false == isright)
+                    {
+                        bellRotation = -1;
+                        bell.transform.Rotate(new Vector3(0, 0, bellRotation));
+                    }
+                }
+
+                bellspd = 0;
+
+                if (0 <= bellRotation) isright = false;
+                else if (0 >= bellRotation) isright = true;
+
+                if (0 >= bellRotation) bellRotation *= -1;
+
+                bellPower = ((bellRotation + targetRotation) * (bellRotation + targetRotation) / (8 * bellRotation * ringTime));
+                bellPower2 = ((bellRotation + targetRotation) * (bellRotation + targetRotation) / (12 * targetRotation * ringTime));
+
+                beforeRotation = targetRotation;
+                
             }
         }
-        
-        // 흔들리는 힘 증가
-        power += arrow;
-
-        // 흔들리는 힘 최대치
-        if(hitCount != 0)
-        {
-            if (power > (4f - playerStatus.currentHP * 0.02f) * hitCount)
-                power = (4f - playerStatus.currentHP * 0.02f) * hitCount;
-            else if (power < -((4f - playerStatus.currentHP * 0.02f) * hitCount))
-                power = -(4f - playerStatus.currentHP * 0.02f) * hitCount;
-        }
-        else
-        {
-            power = 0f;
-
-            if (bell.transform.rotation.z > 0)
-            {
-                power = -1f;
-            }
-            else if (bell.transform.rotation.z < 0)
-                power = 1f;
-
-            if (power < 0 && bell.transform.rotation.z <= 0)
-            {
-                bell.transform.Rotate(new Vector3(0f, 0f, 0f));
-            }
-            else if(power > 0 && bell.transform.rotation.z >= 0)
-            {
-                bell.transform.Rotate(new Vector3(0f, 0f, 0f));
+        else {
+            if (1 >= bellRotation) {
+                bellPower = 0;
+                bellPower2 = 0;
+                beforeRotation = 0;
+                bellspd = 0;
+                bell.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
             }
         }
 
-        bell.transform.Rotate(new Vector3(0, 0, power * 0.2f));
+
+
+        bellRotation = bell.transform.rotation.eulerAngles.z;
+        if (180 <= bellRotation) bellRotation -= 360;
+        //속도 계산 4개의 면이 존재함
+        if (0 <= bellRotation)
+        {
+            if (true == isright) bellspd -= bellPower2 * Time.deltaTime;
+            else bellspd -= bellPower * Time.deltaTime;
+        }
+        else {
+            if (true == isright) bellspd += bellPower * Time.deltaTime;
+            else bellspd += bellPower2 * Time.deltaTime;
+        }
+
+        //벨 실제로 움직임
+        bell.transform.Rotate(new Vector3(0, 0, bellspd));        
     }
 
     // 피격 후 안정화
@@ -108,12 +144,9 @@ public class MainUI_PlayerStatusView : MonoBehaviour
     {
         yield return new WaitForSeconds(5f - playerStatus.GetRecovery_Result() * 0.02f);
         
-        hitCount -= 2f;
-
-        if (hitCount <= 0f)
-            hitCount = 0f;
-        else
-        {
+        targetRotation -= dmgRecovery;
+        if (0 >= targetRotation) targetRotation = 0;
+        else {
             monsterHit = MonsterHit();
             StartCoroutine(monsterHit);
         }
@@ -121,16 +154,10 @@ public class MainUI_PlayerStatusView : MonoBehaviour
 
     public void Hit(float monsterAtk)
     {
-        ++hitCount;
-        if (hitCount > 5)
-            hitCount = 5f;
+        Debug.Log("Hit monster atk UI test Damage : "+monsterAtk);
+        targetRotation += monsterAtk * dmgMulti;
+        if (maxBellRotation <= targetRotation) targetRotation = maxBellRotation;
 
-        // 몬스터 공격력에 따른 흔들림
-        if (power >= 0)
-            power += monsterAtk * hitCount * 0.2f;
-        else if (power < 0)
-            power -= monsterAtk * hitCount * 0.2f;
-        
         StopCoroutine(monsterHit);
         monsterHit = MonsterHit();
         StartCoroutine(monsterHit);
